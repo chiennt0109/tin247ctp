bd22ef4f15dde0893318eab0d4ddc4628e0c65f8
chiennt0109 <chiennt0109@gmail.com>
2025-10-21 21:47:59 +0700

    Update urls.py

--- oj/urls.py
+++ oj/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.shortcuts import render
+
+
+def home(request):
+return render(request, "home.html")
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.submit_code, name='submit'),
+path("", home, name="home"),
+path("admin/", admin.site.urls),
+path("accounts/", include("accounts.urls")),
+path("problems/", include("problems.urls")),
+path("submissions/", include("submissions.urls")),
+path("contests/", include("contests.urls")),
 ]
