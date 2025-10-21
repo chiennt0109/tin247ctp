5dab5b9ea3b0c1de95d31e4d69cd411a9dbef120
chiennt0109 <chiennt0109@gmail.com>
2025-10-21 21:46:19 +0700

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
-# --- Render build script for Django ---
-set -o errexit  # dừng nếu có lỗi
+set -o errexit
 
-echo "⚙️ Installing dependencies..."
+
 pip install -r requirements.txt
-
-echo "📦 Running migrations..."
-python manage.py makemigrations --noinput || true
-python manage.py migrate --noinput
-
-echo "🎨 Collecting static files..."
 python manage.py collectstatic --noinput || true
-
-echo "✅ Build step finished."
+python manage.py migrate --noinput
