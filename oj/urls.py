3d9b3d3f6a9ff6704e7bed9fa45f3e84c58757ab
chiennt0109 <chiennt0109@gmail.com>
2025-10-24 22:55:29 +0700

    Update urls.py

--- oj/urls.py
+++ oj/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path("problems/", include("problems.urls")),
     path("submissions/", include("submissions.urls")),
     path("contests/", include("contests.urls")),
+    # 🌈 Lộ trình học
+    path("roadmap/stage/<int:stage_id>/", views.roadmap_stage, name="roadmap_stage"),
+    path("roadmap/run/", views.run_code_online, name="run_code_online"),  # Ajax chạy code
 ]
 
