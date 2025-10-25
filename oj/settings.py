fdab16f76a11e755f98f13ee1ce75d7763b71d6f
chiennt0109 <chiennt0109@gmail.com>
2025-10-26 06:14:58 +0700

    Update settings.py

--- oj/settings.py
+++ oj/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "allauth.account.middleware.AccountMiddleware",  # ✅ thêm dòng này
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
