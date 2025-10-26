0f1280a1076a4778b892f82681e6abd9686d92c1
chiennt0109 <chiennt0109@gmail.com>
2025-10-26 15:34:55 +0700

    Update views.py

--- problems/views.py
+++ problems/views.py
@@ -1,9 +1,28 @@
+# path: problems/views.py
 from django.shortcuts import render, get_object_or_404
-from .models import Problem
+from .models import Problem, Tag
 
 def problem_list(request):
-    return render(request, 'problems/list.html', { 'problems': Problem.objects.all() })
+    problems = Problem.objects.all()
+
+    # Bộ lọc đơn giản
+    difficulty = request.GET.get('difficulty')
+    tag = request.GET.get('tag')
+
+    if difficulty:
+        problems = problems.filter(difficulty=difficulty)
+    if tag:
+        problems = problems.filter(tags__slug=tag)
+
+    context = {
+        'problems': problems.order_by('code'),
+        'tags': Tag.objects.all(),
+        'selected_difficulty': difficulty,
+        'selected_tag': tag,
+    }
+    return render(request, 'problems/list.html', context)
+
 
 def problem_detail(request, pk):
-    p = get_object_or_404(Problem, pk=pk)
-    return render(request, 'problems/detail.html', { 'p': p })
+    problem = get_object_or_404(Problem, pk=pk)
+    return render(request, 'problems/detail.html', {'p': problem})
