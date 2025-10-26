e76f49656400289ff4425db35ed35cf38f759cf0
chiennt0109 <chiennt0109@gmail.com>
2025-10-26 22:46:10 +0700

    Update urls.py

--- submissions/urls.py
+++ submissions/urls.py
@@ -1,8 +1,7 @@
-# path: submissions/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('my/', views.my_submissions, name='my_submissions'),
-    path('<int:problem_id>/submit/', views.submit, name='submit'),
+    path('<int:problem_id>/submit/', views.submission_create, name='submission_create'),
 ]
