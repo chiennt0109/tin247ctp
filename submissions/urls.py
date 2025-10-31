9379af1bf821a15a770a7024d1cea2ce65093771
chiennt0109 <chiennt0109@gmail.com>
2025-10-31 09:28:21 +0700

    Update urls.py

--- submissions/urls.py
+++ submissions/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path("<int:problem_id>/submit/", views.submission_create, name="submission_create"),
     path("<int:submission_id>/", views.submission_detail, name="submission_detail"),
-    path("my/", views.my_submissions, name="my_submissions"),
 ]
