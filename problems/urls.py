84938fbca305877dbd3f2e256c2b054913e6a3df
chiennt0109 <chiennt0109@gmail.com>
2025-11-01 18:17:55 +0700

    Update urls.py

--- problems/urls.py
+++ problems/urls.py
@@ -12,13 +12,17 @@ urlpatterns = [
     path('<int:pk>/ai_recommend/', views.ai_recommend, name="ai_recommend"),
     path('ai_learning_path/', views.ai_learning_path, name="ai_learning_path"),
     path("<int:pk>/ai_hint_random/", views.ai_hint_random, name="ai_hint_random"),
-    
+
+    path("<int:pk>/ai_solution/", views.get_solution, name="ai_solution"),
+    path("<int:pk>/ai_next/", views.get_next_recommendation, name="ai_next"),
+    path("<int:pk>/ai_learn/", views.get_learning_path, name="ai_learning_path"),
+
 
     # AI cho admin
     path('ai_suggest_tags/', views_admin.ai_suggest_tags, name="ai_suggest_tags"),
     path('ai_analyze_problem/', views_admin.ai_analyze_problem, name="ai_analyze_problem"),
 
-    path("admin_ai_generate/", views.admin_ai_generate),
-    path("admin_ai_samples/", views.admin_ai_samples),
-    path("admin_ai_check/", views.admin_ai_check),
+    #path("admin_ai_generate/", views.admin_ai_generate),
+   # path("admin_ai_samples/", views.admin_ai_samples),
+   # path("admin_ai_check/", views.admin_ai_check),
 ]
