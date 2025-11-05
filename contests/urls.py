5a585d14c43e81580c974b402f56ef0567f72b5a
chiennt0109 <chiennt0109@gmail.com>
2025-11-05 22:27:30 +0700

    Update urls.py

--- contests/urls.py
+++ contests/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.contest_list, name='contest_list'),
     path('<int:contest_id>/', views.contest_detail, name='contest_detail'),
     path('<int:contest_id>/rank/', views.contest_rank, name='contest_rank'),
+    path("<int:contest_id>/ai_report/", views.contest_ai_report, name="contest_ai_report"),
 ]
