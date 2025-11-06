f3874d90eb3084a12e856a45da8f0df8a81c276e
chiennt0109 <chiennt0109@gmail.com>
2025-11-06 09:25:23 +0700

    Update urls.py

--- contests/urls.py
+++ contests/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+app_name = "contests" 
 urlpatterns = [
     path('', views.contest_list, name='contest_list'),
     path('<int:contest_id>/', views.contest_detail, name='contest_detail'),
