21838d8349a7bf35679f4832c5a948a10cf2c13f
chiennt0109 <chiennt0109@gmail.com>
2025-11-06 10:19:29 +0700

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
+# path: accounts/urls.py
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import signup
+
+app_name = "accounts"
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
